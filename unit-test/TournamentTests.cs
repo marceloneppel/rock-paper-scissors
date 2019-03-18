225011e598feea5af6cc9e721478e3c4ea79f5b2
Marcelo Henrique Neppel <marcelo.neppel@gmail.com>
2019-03-17 21:16:28 -0300

    Added rps_tournament_winner tests.

---
+++ unit-test/TournamentTests.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using rock_paper_scissors;
+
+namespace unit_test
+{
+    [TestFixture]
+    public class TournamentTests
+    {
+        [Test]
+        public void OneRoundTournamentShouldWork()
+        {
+            var tournament = new List<dynamic>
+            {
+                new List<string>{"Armando", "P"}, new List<string>{"Dave", "S"}
+            };
+            var tournamentWinner = Game.rps_tournament_winner(tournament);
+            Assert.AreEqual(new List<string>{"Dave", "S"}, tournamentWinner);
+        }
+        
+        [Test]
+        public void RichardShouldWinTheExampleTournament()
+        {
+            var tournament = new List<dynamic>
+            {
+                new List<dynamic>
+                {
+                    new List<dynamic>
+                    {
+                        new List<string>{"Armando", "P"}, new List<string>{"Dave", "S"}
+                    },
+                    new List<dynamic>
+                    {
+                        new List<string>{"Richard", "R"}, new List<string>{"Michael", "S"}
+                    }
+                },
+                new List<dynamic>
+                {
+                    new List<dynamic>
+                    {
+                        new List<string>{"Allen", "S"}, new List<string>{"Omer", "P"}
+                    },
+                    new List<dynamic>
+                    {
+                        new List<string>{"David E.", "R"}, new List<string>{"Richard X.", "P"}
+                    }
+                }
+            };
+            var tournamentWinner = Game.rps_tournament_winner(tournament);
+            Assert.AreEqual(new List<string>{"Richard", "R"}, tournamentWinner);
+        }
+        
+        [Test]
+        public void TwoRoundsTournamentShouldWork()
+        {
+            var tournament = new List<dynamic>
+            {
+                new List<dynamic>
+                {
+                    new List<string>{"Armando", "P"}, new List<string>{"Dave", "S"}
+                },
+                new List<dynamic>
+                {
+                    new List<string>{"Richard", "R"}, new List<string>{"Michael", "S"}
+                }
+            };
+            var tournamentWinner = Game.rps_tournament_winner(tournament);
+            Assert.AreEqual(new List<string>{"Richard", "R"}, tournamentWinner);
+        }
+    }
+}
